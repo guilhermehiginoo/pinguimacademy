6ae169563236523b4938153fde92ceb988ab4e93
Guilherme Higino <160296050+guilhermehiginoo@users.noreply.github.com>
2025-03-01 23:11:51 -0300

    Add files via upload

---
+++ 30-12-include-require/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Include e Require</title>
+</head>
+<body>
+
+    <h1>Include e Require</h1>
+    <ul>
+        <li>
+            <a href="/include.php">Include</a>
+        </li>
+        <li>
+            <a href="/require.php">Require</a>
+        </li>
+
+    </ul>
+    <?php
+    // include retorna um warning
+    // o require retorna um erro
+    ?>
+</body>
+</html>
