38e644a6c3e68d4d530ccb40409ef4702f8d76dc
Guilherme Higino <160296050+guilhermehiginoo@users.noreply.github.com>
2025-03-01 23:10:10 -0300

    Add files via upload

---
+++ 30-11-estrutura-de-repeticao/index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Estruturas de Repetição</title>
+    </head>
+    <body>
+        <h1>Estruturas de Repetição</h1>
+        <ul>
+           <li>
+                <a href="for.php">For</a>
+           </li>
+            <li>
+                <a href="foreach.php">Foreach</a>
+            </li>
+            <li>
+                <a href="while.php">While</a>
+            </li>
+            <li>
+                <a href="do-while.php">Do/While</a>
+            </li>
+        </ul>
+    </body>
+</html>
