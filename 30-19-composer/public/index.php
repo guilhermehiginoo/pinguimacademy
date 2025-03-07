cf6a0ef2c180b17d77e90e1e2415a40191e486a0
Guilherme Higino <guilhermehiginoo@gmail.com>
2025-03-06 21:52:44 -0300

    Adds option to long an event on the index page

--- 30-19-composer/public/index.php
+++ 30-19-composer/public/index.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 require __DIR__ . '/../vendor/autoload.php';
 
 use App\Greetings;
+use App\Logger;
 
 $greetings = new Greetings();
 echo $greetings->message('FLAMENGOOOOO');
+
+(new Logger())->write('Flamengo acessou a aplicação');
